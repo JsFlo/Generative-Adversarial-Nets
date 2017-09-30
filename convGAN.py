eb6dd9131771c5d642de5fcc47876efbf66d919a
Jose Flores <jsflores480@gmail.com>
2017-09-30 17:24:36 -0500

    Adds constants

--- convGAN.py
+++ convGAN.py
@@ -5,6 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+TRAINING_STEPS = 30
+SAMPLE_FREQUENCY = 10
+DEBUG_PRINT_FREQUENCY = 10000
+SAMPLE_WIDTH = 3
+SAMPLE_HEIGHT = 3
+NUMBER_OF_SAMPLES = SAMPLE_WIDTH * SAMPLE_HEIGHT
+FILE_SAMPLE_OUTPUT_PATH = "out/"
+BATCH_SIZE = 128
+
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -36,13 +45,13 @@ disc_biases = {
     'bout': weight_variable([10])
 }
 
-gen_weights = {  # takes in 100
-    'wc1': weight_variable([2, 2, 1, 64]),  # 14 x 14 x 32
-    'wc2': weight_variable([3, 3, 64, 96]),  # 6 x 6 x 64
-    'wc3': weight_variable([2, 2, 96, 128]),  # 3 x 3 x 128 (= 1152 after flatten)
-    'wf1': weight_variable([1 * 1 * 128, 1024]),  # 1152 -> 1024
-    'wf2': weight_variable([1024, 900]),  # 1024 -> 512
-    'out': weight_variable([900, 784])  # 512 -> 1
+gen_weights = {  # takes in 100 -> 10 x 10
+    'wc1': weight_variable([2, 2, 1, 64]),  # 5 x 5 x 64
+    'wc2': weight_variable([3, 3, 64, 96]),  # 2 x 2 x 96
+    'wc3': weight_variable([2, 2, 96, 128]),  # 1 x 1 x 128 (= 128 after flatten)
+    'wf1': weight_variable([1 * 1 * 128, 1024]),  # 128 -> 1024
+    'wf2': weight_variable([1024, 900]),  # 1024 -> 900
+    'out': weight_variable([900, 784])  # 900 -> 784
 }
 
 gen_biases = {
@@ -88,20 +97,13 @@ def getHiddenLayer(lastLayer, weight, bias):
     return max_pool_2x2(convLayer)
 
 
-# used for printing accuracy, sets the dropout to 1 (no droput)
-def printAccuracy(accuracy, step, inputPlaceholder, correctLabelPlaceholder, inputs, correctLabels, keep_prob):
-    train_accuracy = accuracy.eval(
-        feed_dict={inputPlaceholder: inputs, correctLabelPlaceholder: correctLabels, keep_prob: 1.0})
-    print('step %d, training accuracy %g' % (step, train_accuracy))
-
-
 def printShape(tensor):
     print(tensor.shape)
 
 
 def plot(samples):
-    fig = plt.figure(figsize=(4, 4))
-    gs = gridspec.GridSpec(4, 4)
+    fig = plt.figure(figsize=(SAMPLE_WIDTH, SAMPLE_HEIGHT))
+    gs = gridspec.GridSpec(SAMPLE_WIDTH, SAMPLE_HEIGHT)
     gs.update(wspace=0.05, hspace=0.05)
 
     for i, sample in enumerate(samples):
@@ -110,6 +112,7 @@ def plot(samples):
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_aspect('equal')
+        # convert 784 to 28 x 28
         plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
 
     return fig
@@ -118,32 +121,29 @@ def plot(samples):
 # expects x to be of shape 28 x 28
 def getCNN(x, keep_prob, weights, bias):
     # 3 hidden layers
-    print("3 hidden")
     conv1 = getHiddenLayer(x, weights['wc1'], bias['bc1'])
-    printShape(conv1)
     conv2 = getHiddenLayer(conv1, weights['wc2'], bias['bc2'])
-    printShape(conv2)
     conv3 = getHiddenLayer(conv2, weights['wc3'], bias['bc3'])
-    printShape(conv3)
     # flatten 3 to go into fully connected
     conv3_flattened = tf.reshape(conv3, [-1, bias['reshape']])
-    printShape(conv3_flattened)
-    print("fully connected")
     # fully connected 1 with dropout
     fullyConnected1 = getFullyConnectedLayer(conv3_flattened, weights['wf1'], bias['bf1'])
     fullyConnected1_dropout = tf.nn.dropout(fullyConnected1, keep_prob)
-    printShape(fullyConnected1)
     # fully connected 2
     fullyConnected2 = getFullyConnectedLayer(fullyConnected1_dropout, weights['wf2'], bias['bf2'])
-    printShape(fullyConnected2)
     # fully connected 3 no relu applied
     fullyConnected3_logit = getFullyConnectedLayer(fullyConnected2, weights['out'], bias['bout'], False)
     fullyConnected3_prob = tf.nn.sigmoid(fullyConnected3_logit)
-    printShape(fullyConnected3_logit)
     return fullyConnected3_prob, fullyConnected3_logit
 
 
 def main():
+    if not os.path.exists(FILE_SAMPLE_OUTPUT_PATH):
+        os.makedirs(FILE_SAMPLE_OUTPUT_PATH)
+
+    # going to be randomly generated
+    numberOfInputs = 100
+
     # used for dropout later, hold a ref so we can remove it during testing
     disc_real_keep_prob = tf.placeholder(tf.float32)
     disc_fake_keep_prob = tf.placeholder(tf.float32)
@@ -160,7 +160,7 @@ def main():
     x_image = tf.reshape(x, [-1, 28, 28, 1])
 
     # input to the generator of 100 numbers of noise
-    Z = tf.placeholder(tf.float32, shape=[None, 100])
+    Z = tf.placeholder(tf.float32, shape=[None, numberOfInputs])
     Z_reshaped = tf.reshape(Z, [-1, 10, 10, 1])
     # generator will take in Z (random noise of 100) and output an image that's 28 x 28
     G_sample, _ = getCNN(Z_reshaped, gen_keep_prob, gen_weights, gen_biases)
@@ -189,36 +189,30 @@ def main():
 
     G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
 
-    mb_size = 128
-    Z_dim = 100
-
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    if not os.path.exists('out/'):
-        os.makedirs('out/')
+    for it in range(TRAINING_STEPS):
 
-    i = 0
-
-    for it in range(100):
-        if it % 10 == 0:
-            samples = sess.run(G_sample, feed_dict={Z: sample_Z(16, Z_dim), disc_fake_keep_prob: 1.0,
-                                             disc_real_keep_prob: 1.0, gen_keep_prob: 1.0})
-
+        if it % SAMPLE_FREQUENCY == 0:
+            # sample the generator and save the plots
+            samples = sess.run(G_sample,
+                               feed_dict={Z: sample_Z(NUMBER_OF_SAMPLES, numberOfInputs), disc_fake_keep_prob: 1.0,
+                                          disc_real_keep_prob: 1.0, gen_keep_prob: 1.0})
             fig = plot(samples)
-            plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
-            i += 1
+            plt.savefig('{}{}.png'.format(FILE_SAMPLE_OUTPUT_PATH, str(it).zfill(3)), bbox_inches='tight')
             plt.close(fig)
 
-        X_mb, _ = mnist.train.next_batch(mb_size)
+        X_mb, _ = mnist.train.next_batch(BATCH_SIZE)
 
         _, D_loss_curr = sess.run([D_solver, D_loss],
-                                  feed_dict={x: X_mb, Z: sample_Z(mb_size, Z_dim), disc_fake_keep_prob: 1.0,
-                                             disc_real_keep_prob: 1.0, gen_keep_prob: 1.0})
-        _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: sample_Z(mb_size, Z_dim), disc_fake_keep_prob: 1.0,
-                                                                 disc_real_keep_prob: 1.0, gen_keep_prob: 1.0})
+                                  feed_dict={x: X_mb, Z: sample_Z(BATCH_SIZE, numberOfInputs),
+                                             disc_fake_keep_prob: 1.0, disc_real_keep_prob: 1.0, gen_keep_prob: 1.0})
+        _, G_loss_curr = sess.run([G_solver, G_loss],
+                                  feed_dict={Z: sample_Z(BATCH_SIZE, numberOfInputs),
+                                             disc_fake_keep_prob: 1.0, disc_real_keep_prob: 1.0, gen_keep_prob: 1.0})
 
-        if it % 10 == 0:
+        if it % DEBUG_PRINT_FREQUENCY == 0:
             print('Iter: {}'.format(it))
             print('D loss: {:.4}'.format(D_loss_curr))
             print('G_loss: {:.4}'.format(G_loss_curr))
